1fbebfb72cc5a431da88683361581a0e845fbeee
TWIAV <twiav.nl@gmail.com>
2021-12-29 12:21:56 +0100

    New resolution/zoom levels added for the aerial imagery

--- ol-retain-mapstate-nl-basemaps/main.js
+++ ol-retain-mapstate-nl-basemaps/main.js
@@ -73,9 +73,9 @@ if (window.location.hash !== '') {
 // Tiling schema for the Netherlands (Amersfoort / RD New): EPSG:28992
 const proj28992Extent = [-285401.92, 22598.08, 595401.92, 903401.92];
 const proj28992 = new Projection({ code: 'EPSG:28992', units: 'm', extent: proj28992Extent });
-const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210];
+const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525, 0.02625, 0.013125, 0.0065625];
 const matrixIds = [];
-for (let i = 0; i < 15; ++i) {
+for (let i = 0; i < 20; ++i) {
   matrixIds[i] = 'EPSG:28992:' + i;
 }
 
@@ -88,6 +88,7 @@ const dutchWMTSTileGrid = new WMTSTileGrid({
 const openTopoLayer = new TileLayer({
   title: 'OpenTopo',
   type: 'base',
+  minResolution: 0.200,
   visible: false,
   source: new WMTSSource({
     url: 'https://geodata.nationaalgeoregister.nl/tiles/service/wmts',
@@ -100,6 +101,7 @@ const openTopoLayer = new TileLayer({
 const openTopoAchtergrondkaartLayer = new TileLayer({
   title: 'OpenTopo Achtergrondkaart',
   type: 'base',
+  minResolution: 0.200,
   visible: false,
   source: new WMTSSource({
     url: 'https://geodata.nationaalgeoregister.nl/tiles/service/wmts',
@@ -112,6 +114,7 @@ const openTopoAchtergrondkaartLayer = new TileLayer({
 const brtAchtergrondkaartLayer = new TileLayer({
   title: 'BRT Achtergrondkaart',
   type: 'base',
+  minResolution: 0.200,
   visible: false,
   source: new WMTSSource({
     url: 'https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0',
@@ -153,13 +156,16 @@ const baseMaps = new LayerGroup({
 	layers: [luchtfotoActueelOrthoHRRGBLayer, luchtfotoActueelOrtho25cmRGBLayer, brtAchtergrondkaartLayer, openTopoAchtergrondkaartLayer, openTopoLayer]
 });
 
+const minZoom = 0;
+const maxZoom = 19;
+
 const map = new Map({
   layers: [
     baseMaps
   ],
   controls: defaultControls({attribution: false}).extend([new CopyUrlControl(), attribution]),
   target: 'map',
-  view: new View({minZoom: 0, maxZoom: 15, projection: proj28992, center: center, zoom: zoom})
+  view: new View({minZoom: minZoom, maxZoom: maxZoom, projection: proj28992, center: center, zoom: zoom})
 })
 
 const layerSwitcher = new LayerSwitcher({
@@ -171,6 +177,19 @@ const layerSwitcher = new LayerSwitcher({
 });
 
 map.addControl(layerSwitcher);
+
+map.on ('moveend', handleZoomBtnsAndLayerSwitcher);
+
+function handleZoomBtnsAndLayerSwitcher(evt) {
+  const zoomLevel = Math.round(map.getView().getZoom());
+  const zoomInBtn = document.querySelector(".ol-zoom-in");
+  const zoomOutBtn = document.querySelector(".ol-zoom-out");
+  // Gray out zoom buttons at maximum and minimum zoom respectively
+  zoomLevel === maxZoom ? zoomInBtn.style.backgroundColor = "rgba(0,60,136,0.1)" : zoomInBtn.style.backgroundColor = "rgba(0,60,136,0.5)";
+  zoomLevel === minZoom ? zoomOutBtn.style.backgroundColor = "rgba(0,60,136,0.1)" : zoomOutBtn.style.backgroundColor = "rgba(0,60,136,0.5)";
+  // Make sure the layer switcher is rerendered to set the color (gray or black) for layer titles, depending on their visibility at a certain zoomlevel
+  layerSwitcher.renderPanel();
+}
 
 openTopoLayer.on('change:visible', function(){ updateURLHash() });
 openTopoAchtergrondkaartLayer.on('change:visible', function(){ updateURLHash() });
